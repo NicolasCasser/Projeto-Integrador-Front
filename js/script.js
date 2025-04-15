788018ad92b11e60158251391fe718135b2159f2
NicolasCasser <52513004+NicolasCasser@users.noreply.github.com>
2025-04-15 16:14:22 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 // Função chamada quando o botão "Acessar" é clicado
-function enviar() {
-    // Previne o envio do formulário
-    event.preventDefault();
+// function enviar() {
+//     // Previne o envio do formulário
+//     e.preventDefault();
 
-    // Obtém os valores dos campos de entrada
-    let nomeUsuario = document.querySelector(".nome input").value;
-    let email = document.querySelector(".email input").value;
-    let senha = document.querySelector("#senha").value;
-    let confirmarSenha = document.querySelector("#cSenha").value;
-    let mErro = document.getElementById("mErro"); // Div onde as mensagens de erro são exibidas
+//     // Obtém os valores dos campos de entrada
+//     let nomeUsuario = document.querySelector(".nome input").value;
+//     let email = document.querySelector(".email input").value;
+//     let senha = document.querySelector("#senha").value;
+//     let confirmarSenha = document.querySelector("#cSenha").value;
+//     let mErro = document.getElementById("mErro"); // Div onde as mensagens de erro são exibidas
 
-    mErro.innerHTML = "";
+//     mErro.innerHTML = "";
 
-    // Validação dos campos de entrada
-    if (!nomeUsuario || !email || !senha || !confirmarSenha) {
-        mErro.innerHTML = "<p style='color: #f4f4f4;'>Todos os campos são obrigatórios!</p>";
-        return;
-    }
+//     // Validação dos campos de entrada
+//     if (!nomeUsuario || !email || !senha || !confirmarSenha) {
+//         mErro.innerHTML = "<p style='color: #f4f4f4;'>Todos os campos são obrigatórios!</p>";
+//         return;
+//     }
 
-    if (senha !== confirmarSenha) {
-        mErro.innerHTML = "<p style='color: #f4f4f4;'>As senhas não coincidem!</p>";
-        return;
-    }
+//     if (senha !== confirmarSenha) {
+//         mErro.innerHTML = "<p style='color: #f4f4f4;'>As senhas não coincidem!</p>";
+//         return;
+//     }
 
-    mErro.innerHTML = "<p style='color: #f4f4f4;'>Cadastro realizado com sucesso!</p>";
-}
+//     mErro.innerHTML = "<p style='color: #f4f4f4;'>Cadastro realizado com sucesso!</p>";
+// }
 
 
 function valida() {
@@ -46,3 +46,71 @@ function valida() {
         iSenha.style.border = ("solid, 2px, green")
     }
 }
+
+const cadastro = document.getElementById("formRegistro");
+console.log(cadastro);
+
+cadastro.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const usuario = {
+        nome,
+        email,
+        senha
+    };
+    console.log(usuario);
+    
+
+    const response = await fetch('http://localhost:1212/registro', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+    });
+
+    
+    if(response.ok) {
+        alert('Usuário cadastrado com sucesso!');
+    } else {
+        alert('Erro ao cadastrar usuário!');
+    }
+})
+
+const login = document.getElementById("formLogin");
+console.log(login);
+
+
+login.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("emailLogin").value;
+    const senha = document.getElementById("senhaLogin").value;
+    
+
+    console.log(email);
+    
+    
+    const usuario = {
+        email,
+        senha
+    };
+
+    console.log(usuario);
+    
+    const response = await fetch("http://localhost:1212/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(usuario)
+    });
+
+    if(response.ok){
+        window.location.href = "../Navegacao/Nav.html"
+    }else {
+        alert("Erro ao fazer login!");
+    }
+});
